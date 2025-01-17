0987d0be85a67405d99313b51b1b914f039a872e
Nana Mohamadi <adilnana78@gmail.com>
2025-01-18 00:21:27 +0100

    Homepage Layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,15 +1,34 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import MobileNav from "@/components/MobileNav";
+import RightSidebar from "@/components/RightSidebar";
+import Image from "next/image";
 import React from "react";
 
 const Layout = async ({
   children,
 }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <div>
-      <main>
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
         <LeftSidebar />
-        {children}
-        <p className="text-white-1">RIGHT SIDEBAR</p>
+        <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">
+              <Image
+                src="/icons/logo.svg"
+                alt="menu logo"
+                height={30}
+                width={30}
+              />
+             <MobileNav />
+            </div>
+            <div className="flex flex-col md:pb-14">
+              Toaster
+              {children}
+            </div>
+          </div>
+        </section>
+        <RightSidebar />
       </main>
     </div>
   );
